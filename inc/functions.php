8dfd0389560e22f1bde1051cbca90d2be7b47fba
rabindratharu <rabindra.tharu.np@gmail.com>
2016-12-01 16:34:56 +0545

    fixes migrate custom css hook function

--- inc/functions.php
+++ inc/functions.php
@@ -575,21 +575,32 @@ function accelerate_wrapper_start() {
 function accelerate_wrapper_end() {
   echo '</div>';
 }
+
 /**
  * Migrate any existing theme CSS codes added in Customize Options to the core option added in WordPress 4.7
  */
- function accelerate_custom_css_migrate() {
-     if ( function_exists( 'wp_update_custom_css_post' ) ) {
-         $custom_css = accelerate_options( 'accelerate_custom_css' );
-         if ( $custom_css ) {
-             $core_css = wp_get_custom_css(); // Preserve any CSS already added to the core option.
-             $return = wp_update_custom_css_post( $core_css . $custom_css );
-             if ( ! is_wp_error( $return ) ) {
-                 // Remove the old theme_mod, so that the CSS is stored in only one place moving forward.
-                 remove_theme_mod( 'accelerate_custom_css' );
-             }
-         }
-     }
- }
- add_action( 'after_setup_theme', 'accelerate_custom_css_migrate' );
-?>
+function accelerate_custom_css_migrate() {
+	if ( function_exists( 'wp_update_custom_css_post' ) ) {
+		$custom_css = accelerate_options( 'accelerate_custom_css' );
+		if ( $custom_css ) {
+			$core_css = wp_get_custom_css(); // Preserve any CSS already added to the core option.
+			$return = wp_update_custom_css_post( $core_css . $custom_css );
+
+			if ( ! is_wp_error( $return ) ) {
+
+				$theme_options = get_option( 'accelerate' );
+
+				// Remove the old theme_mod, so that the CSS is stored in only one place moving forward.
+				foreach ( $theme_options as $option_key => $option_value ) {
+					if ( in_array( $option_key, array( 'accelerate_custom_css' ) ) ) {
+						unset( $theme_options[ $option_key ] );
+					}
+				}
+				// Finally, update accelerate theme options.
+				update_option( 'accelerate', $theme_options );
+			}
+		}
+	}
+}
+
+add_action( 'after_setup_theme', 'accelerate_custom_css_migrate' );

826cfc0b0de2744dee175c71f89005c220bb57a5
Nima Lama <nima.themegrill@gmail.com>
2017-05-29 11:08:51 +0545

    Implemented bower for auto update of library file

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,6 +88,30 @@ module.exports = function( grunt ){
 				dest: 'accelerate',
 				expand: true
 			}
+		},
+		// Copy
+		copy: {
+			facss: {
+				files: [{
+					cwd: 'bower_components/font-awesome/css',  // set working folder / root to copy
+					src: '**/*.css',           // copy all files and subfolders
+					dest: 'fontawesome/css/',    // destination folder
+					expand: true           // required when using cwd
+				}]
+			},
+			fafonts: {
+				files: [{
+					cwd: 'bower_components/font-awesome/fonts',  // set working folder / root to copy
+					src: '**/*',           // copy all files and subfolders
+					dest: 'fontawesome/fonts/',    // destination folder
+					expand: true           // required when using cwd
+				}]
+			}
+		},
+		bower: {
+			update: {
+				//just run 'grunt bower:install' and you'll see files from your Bower packages in lib directory
+			}
 		}
 	});
 
@@ -96,10 +120,17 @@ module.exports = function( grunt ){
 	grunt.loadNpmTasks( 'grunt-checktextdomain' );
 	grunt.loadNpmTasks( 'grunt-contrib-compress' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-bower-task' );
 
 	// Register tasks
 	grunt.registerTask( 'default', [
 		'jshint'
+	]);
+
+	grunt.registerTask( 'update', [
+		'bower',
+		'copy',
 	]);
 
 	grunt.registerTask( 'dev', [

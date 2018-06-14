e41e71e02fac65be5e2c9b137418350b5b790229
Ashish Shrestha <ashishsthanp@gmail.com>
2018-06-14 11:35:04 +0545

    Code reformat

--- js/navigation.js
+++ js/navigation.js
@@ -4,19 +4,20 @@
  * Handles toggling the navigation menu for small screens.
  */
 ( function() {
+
 	var container, button, menu;
 
 	container = document.getElementById( 'site-navigation' );
-	if ( ! container ) {
+	if ( !container ) {
 		return;
 	}
 
-	button = container.getElementsByTagName( 'h3' )[0];
+	button = container.getElementsByTagName( 'h3' )[ 0 ];
 	if ( 'undefined' === typeof button ) {
 		return;
 	}
 
-	menu = container.getElementsByTagName( 'ul' )[0];
+	menu = container.getElementsByTagName( 'ul' )[ 0 ];
 
 	// Hide menu toggle button if menu is empty and return early.
 	if ( 'undefined' === typeof menu ) {
@@ -35,49 +36,58 @@
 			container.className = container.className.replace( 'main-navigation', 'main-small-navigation' );
 		}
 	};
+
 } )();
-jQuery(document).ready(function() {
-    jQuery('#site-navigation .menu-item-has-children').append('<span class="sub-toggle"> <i class="fa fa-caret-down"></i> </span>');
-    jQuery('#site-navigation .sub-toggle').click(function() {
-        jQuery(this).parent('.menu-item-has-children').children('ul.sub-menu').first().slideToggle('1000');
-        jQuery(this).children('.fa-caret-right').first().toggleClass('fa-caret-down');
-        jQuery(this).toggleClass('active');
-    });
-});
+jQuery( document ).ready( function() {
+
+	jQuery( '#site-navigation .menu-item-has-children' ).append( '<span class="sub-toggle"> <i class="fa fa-caret-down"></i> </span>' );
+
+	jQuery( '#site-navigation .sub-toggle' ).click( function() {
+
+		jQuery( this ).parent( '.menu-item-has-children' ).children( 'ul.sub-menu' ).first().slideToggle( '1000' );
+		jQuery( this ).children( '.fa-caret-right' ).first().toggleClass( 'fa-caret-down' );
+		jQuery( this ).toggleClass( 'active' );
+
+	} );
+
+} );
 
 // Show Submenu on click on touch enabled deviced
-( function () {
-    var container;
-    container = document.getElementById( 'site-navigation' );
+( function() {
 
-    /**
-     * Toggles `focus` class to allow submenu access on tablets.
-     */
-    ( function( container ) {
-        var touchStartFn, i,
-            parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+	var container = document.getElementById( 'site-navigation' );
 
-        if ( 'ontouchstart' in window ) {
-            touchStartFn = function( e ) {
-                var menuItem = this.parentNode, i;
+	/**
+	 * Toggles `focus` class to allow submenu access on tablets.
+	 */
+	( function( container ) {
 
-                if ( ! menuItem.classList.contains( 'focus' ) ) {
-                    e.preventDefault();
-                    for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
-                        if ( menuItem === menuItem.parentNode.children[i] ) {
-                            continue;
-                        }
-                        menuItem.parentNode.children[i].classList.remove( 'focus' );
-                    }
-                    menuItem.classList.add( 'focus' );
-                } else {
-                    menuItem.classList.remove( 'focus' );
-                }
-            };
+		var touchStartFn, i,
+		    parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
 
-            for ( i = 0; i < parentLink.length; ++i ) {
-                parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
-            }
-        }
-    }( container ) );
-} ) ();
+		if ( 'ontouchstart' in window ) {
+			touchStartFn = function( e ) {
+				var menuItem = this.parentNode, i;
+
+				if ( !menuItem.classList.contains( 'focus' ) ) {
+					e.preventDefault();
+					for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
+						if ( menuItem === menuItem.parentNode.children[ i ] ) {
+							continue;
+						}
+						menuItem.parentNode.children[ i ].classList.remove( 'focus' );
+					}
+					menuItem.classList.add( 'focus' );
+				} else {
+					menuItem.classList.remove( 'focus' );
+				}
+			};
+
+			for ( i = 0; i < parentLink.length; ++i ) {
+				parentLink[ i ].addEventListener( 'touchstart', touchStartFn, false );
+			}
+		}
+
+	}( container ) );
+
+} )();

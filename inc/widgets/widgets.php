fa5f98e46dfe1c80932f8b6c3326ef27436bea64
Nitu Shrestha <shresthanitu984@gmail.com>
2018-07-12 11:52:15 +0545

    Use seperate file for custom tag widget

--- inc/widgets/widgets.php
+++ inc/widgets/widgets.php
@@ -132,54 +132,5 @@ require ACCELERATE_WIDGETS_DIR . '/accelerate-recent-work-widget.php';
 // Require file for TG: Image Services.
 require ACCELERATE_WIDGETS_DIR . '/accelerate-image-service-widget.php';
 
-
-/**
- * ThemeGrill Custom Tag Widget
- */
-class accelerate_custom_tag_widget extends WP_Widget {
-	function __construct() {
-		$widget_ops = array(
-			'classname'                   => 'accelerate_tagcloud_widget',
-			'description'                 => __( 'Custom Tag Cloud', 'accelerate' ),
- 			'customize_selective_refresh' => true,
-		);
-		$control_ops = array( 'width' => 200, 'height' => 250 );
-		parent::__construct( false, $name = __( 'TG: Custom Tag Cloud', 'accelerate' ) , $widget_ops, $control_ops );
-	}
-
-	function widget( $args, $instance ) {
-		extract( $args );
-		extract( $instance );
-		$title = empty( $instance[ 'title' ] ) ? 'Tags' : $instance[ 'title' ];
-
-		echo $before_widget;
-
-		if ( $title ):
-			echo $before_title . $title . $after_title;
-		endif;
-
-		wp_tag_cloud( 'smallest=13&largest=13px&unit=px' );
-
-		echo $after_widget;
-	}
-	function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-
-		return $instance;
-	}
-
-	function form($instance) {
-		$instance = wp_parse_args( ( array ) $instance, array( 'title'=>'Tags' ) );
-		$title = esc_attr( $instance[ 'title' ] );
-		?>
-
-		<p>
-		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:', 'accelerate' ); ?></label>
-		<input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
-		</p>
-	<?php
-	}
-}
-
-?>
+// Require file for TG: Custom Tag Cloud.
+require ACCELERATE_WIDGETS_DIR . '/accelerate-custom-tag-widget.php';

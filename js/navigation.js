b8a3d347ba669c7d8d28d64ea52a3e6b537be826
Bishal Napit <mebishalnapit@gmail.com>
2019-08-13 10:15:57 +0545

    Add - Focus class addition for menu on keyboard Tab button press

--- js/navigation.js
+++ js/navigation.js
@@ -5,7 +5,7 @@
  */
 ( function() {
 
-	var container, button, menu;
+	var container, button, menu, links, i, len;
 
 	container = document.getElementById( 'site-navigation' );
 	if ( !container ) {
@@ -36,6 +36,36 @@
 			container.className = container.className.replace( 'main-navigation', 'main-small-navigation' );
 		}
 	};
+
+	// Get all the link elements within the menu.
+	links = menu.getElementsByTagName( 'a' );
+
+	// Each time a menu link is focused or blurred, toggle focus.
+	for ( i = 0, len = links.length; i < len; i++ ) {
+		links[i].addEventListener( 'focus', toggleFocus, true );
+		links[i].addEventListener( 'blur', toggleFocus, true );
+	}
+
+	/**
+	 * Sets or removes .focus class on an element.
+	 */
+	function toggleFocus() {
+		var self = this;
+
+		// Move up through the ancestors of the current link until we hit .nav-menu.
+		while ( - 1 === self.className.indexOf( 'nav-menu' ) ) {
+			// On li elements toggle the class .focus.
+			if ( 'li' === self.tagName.toLowerCase() ) {
+				if ( - 1 !== self.className.indexOf( 'focus' ) ) {
+					self.className = self.className.replace( ' focus', '' );
+				} else {
+					self.className += ' focus';
+				}
+			}
+
+			self = self.parentElement;
+		}
+	}
 
 } )();
 jQuery( document ).ready( function() {

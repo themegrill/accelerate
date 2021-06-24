751ed6f9577165990d6818f18a14013155f0a051
ankitkarki17 <ankitkarki17@gmail.com>
2021-06-24 16:49:10 +0545

    Tweak - Update old links of TG sites

--- inc/admin/class-accelerate-dashboard.php
+++ inc/admin/class-accelerate-dashboard.php
@@ -87,7 +87,7 @@ class Accelerate_Dashboard {
 						<h3><?php esc_html_e( 'Next Steps', 'accelerate' ); ?></h3>
 						<ul>
 							<li><?php printf( '<a target="_blank" href="%s" class="welcome-icon dashicons-media-text">' . esc_html__( 'Documentation', 'accelerate' ) . '</a>', esc_url( 'https://docs.themegrill.com/accelerate' ) ); ?></li>
-							<li><?php printf( '<a target="_blank" href="%s" class="welcome-icon dashicons-layout">' . esc_html__( 'Starter Demos', 'accelerate' ) . '</a>', esc_url( 'https://demo.themegrill.com/accelerate-demos' ) ); ?></li>
+							<li><?php printf( '<a target="_blank" href="%s" class="welcome-icon dashicons-layout">' . esc_html__( 'Starter Demos', 'accelerate' ) . '</a>', esc_url( 'https://themegrilldemos.com/accelerate-demos/' ) ); ?></li>
 							<li><?php printf( '<a target="_blank" href="%s" class="welcome-icon dashicons-migrate">' . esc_html__( 'Premium Version', 'accelerate' ) . '</a>', esc_url( 'https://themegrill.com/themes/accelerate' ) ); ?></li>
 						</ul>
 					</div>

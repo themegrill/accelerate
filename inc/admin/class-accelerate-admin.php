f0e734ea1dc9a5a0cbfe57b898a184f5fd95cf35
som shrestha <109511507+som3669@users.noreply.github.com>
2025-08-12 10:21:28 +0545

    fix security capability issue (#56)

--- inc/admin/class-accelerate-admin.php
+++ inc/admin/class-accelerate-admin.php
@@ -36,8 +36,13 @@ if ( ! class_exists( 'Accelerate_admin' ) ) :
 			$welcome_data = array(
 				'uri'      => esc_url( admin_url( '/themes.php?page=demo-importer&browse=all&accelerate-hide-notice=welcome' ) ),
 				'btn_text' => esc_html__( 'Processing...', 'accelerate' ),
-				'nonce'    => wp_create_nonce( 'accelerate_demo_import_nonce' ),
 			);
+
+			// Only add nonce and ajaxurl if user has appropriate capabilities
+			if ( current_user_can( 'manage_options' ) ) {
+				$welcome_data['nonce']   = wp_create_nonce( 'accelerate_demo_import_nonce' );
+				$welcome_data['ajaxurl'] = admin_url( 'admin-ajax.php' );
+			}
 
 			wp_localize_script( 'accelerate-plugin-install-helper', 'accelerateRedirectDemoPage', $welcome_data );
 		}
